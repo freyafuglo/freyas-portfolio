e04aa64f091604f96212e638afdf78fa690428fa
freyafuglo <frey3246@stud.kea.dk>
2025-09-03 22:11:40 +0200

    feat(page): temporarily removes metadata

--- app/page.js
+++ app/page.js
@@ -2,20 +2,6 @@
 
 import Portfolio from "../components/Portfolio";
 
-//seo
-
-export const metadata = {
-  title: "Freya Fuglø | Portfolio",
-  description: "Portfolio of Freya Fuglø, software engineer from Copenhagen.",
-  keywords: [
-    "Freya Fuglø",
-    "software developer",
-    "portfolio",
-    "projects",
-    "Copenhagen",
-  ],
-};
-
 export default function HomePage() {
   return (
     <main>
